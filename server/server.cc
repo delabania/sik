64e2d40c57a9b74740b730ca7f58204b19af465f
delabania <przem0.b95@gmail.com>
2017-06-04 17:41:58 +0200

    cos tam w serwerze

--- server/server.cc
+++ server/server.cc
@@ -1,9 +1,50 @@
+#include <vector>
+#include <utility>
+#include <cstdlib>
 
+#include "events.h"
+
+using namespace std;
 
 int BOARD_WIDTH = 800;
 int BOARD_HEIGHT = 600;
 int PORT_NUMBER = 12345;
+int ROUNDS_PER_SEC = 50;
+int TURNING_SPEED = 6;
+
+
+class Player {
+	int socket;
+	int session_id;
+}
+
+struct SnakeHead {
+	pair <double, double> position;
+	double moveDirection;
+};
+
+
+class GameState {
+	int game_id;
+
+	vector<Player> players;
+	// pozycje głów węży graczy
+	vector<SnakeHead> playersPositions;
+	// tablica o rozmiarach BOARD_WIDTH x BOARD_HEIGHT
+	vector<vector<int>> occupiedPixels;
+	// wydarzenia od poczatku gry
+	vector<shared_ptr<Event>> events;
+
+
+
+};
+
+
 
 int main() {
+	int game_id = rand();
+
+
+
 
 }
